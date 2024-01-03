30cf6f64562415c6dc8385d7ecf904134d323331
Rita2024 <ritaejezie2@gmail.com>
2024-01-03 15:34:07 +0000

    bblock scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task3 = true;
+    const task4 = false;
+
+    return [task3, task4];
   }
 
   return [task, task2];
